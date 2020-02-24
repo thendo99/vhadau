47b0881924f386c760b80578eb796f8f5c445e67
thendo99 <thendomudau99@gmail.com>
2020-02-24 14:56:29 +0200

    First commit

---
+++ Book.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook
+{
+    class Book
+    {
+
+        public List<double> grades;
+
+
+        public void InsertMarks(double t)
+        {
+
+            grades.Add(t);
+        }
+
+
+
+        public void ShowStatistics()
+        {
+            double average = 0;
+            double high = 0;
+            double low = 0;
+            double sum = 0;
+
+
+            foreach(var number in  grades)
+            {
+                low = Math.Min(number, low);
+                high = Math.Max(number, high);
+                sum += number;
+                
+
+
+                
+
+
+                
+            }
+            average = sum / grades.Count;
+            Console.WriteLine($"The highehst mark is {high}");
+            Console.WriteLine($"The lowest mark is {low}");
+            Console.WriteLine($"The Avergae mark is {average}");
+            
+
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book tt = new Book();
+
+
+            tt.InsertMarks(10);
+            tt.InsertMarks(5);
+            tt.InsertMarks(2);
+            tt.InsertMarks(3);
+            tt.InsertMarks(4);
+
+            tt.ShowStatistics();
+
+
+
+
+
+
+
+
+        }
+    }
+}
